0816d1293cda24474838e0dfcc003546284cb0ae
CL <lee1003094395@gmail.com>
2020-05-26 21:18:52 +0800

    update OrderDataHandler

--- BookingSystem/src/database_utils/TourDataHandler.java
+++ BookingSystem/src/database_utils/TourDataHandler.java
@@ -13,6 +13,7 @@ import database_utils.TourData;
 public class TourDataHandler {
 	
 	private static String SQ = "SELECT * FROM trip_data WHERE travel_code = ? AND start_date = ?";
+	private static String SQ_ITV = "SELECT * FROM trip_data WHERE travel_code = ? AND start_date >= ? AND start_date <= ?";
 	
 	
 	/** Gets tours from ResultSet.
@@ -33,6 +34,7 @@ public class TourDataHandler {
 				td.setTitle(rs.getString("title"));
 				td.setPrice(rs.getInt("price"));
 				td.setStartDate(rs.getDate("start_date").toLocalDate());
+				td.setEndDate(rs.getDate("end_date").toLocalDate());
 				td.setLowerBound(rs.getInt("lower_bound"));
 				td.setUpperBound(rs.getInt("upper_bound"));
 				tours.add(td);
@@ -71,6 +73,51 @@ public class TourDataHandler {
 			stmt = con.prepareStatement(SQ, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
 			stmt.setInt(1, travelCode);
 			stmt.setString(2, d);
+			rs = stmt.executeQuery();
+			
+			if (!rs.isBeforeFirst()) {  // check if any data exists
+				tourDataList = null;
+				return tourDataList;
+			}
+			
+			tourDataList = getTours(rs);
+			
+			if(rs != null) rs.close();
+			if(stmt != null) stmt.close();
+			if(con != null) con.close();
+			
+		}
+		catch(Exception e) {
+			System.err.print(e);
+		}
+		
+		return tourDataList;
+	}
+	
+	
+	/** getResultFromDatabase interval version, with day1 <= day2
+	 * @return ArrayList of TourData
+	 */
+	public static ArrayList<TourData> getResultFromDatabaseWithInterval(int travelCode,
+			int year1, int month1, int day1, int year2, int month2, int day2) {
+		
+		String d1 = String.format("%04d-%02d-%02d", year1, month1, day1);
+		String d2 = String.format("%04d-%02d-%02d", year2, month2, day2);
+		
+		ArrayList<TourData> tourDataList = null;
+		
+		Connection con = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		
+		try {
+			
+			con = DB_Connection.getConnection();
+			
+			stmt = con.prepareStatement(SQ_ITV, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+			stmt.setInt(1, travelCode);
+			stmt.setString(2, d1);
+			stmt.setString(3, d2);
 			rs = stmt.executeQuery();
 			
 			if (!rs.isBeforeFirst()) {  // check if any data exists
